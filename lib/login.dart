e8ab78a0daa29bd0e969c1d613eaa33d3590e6d1
diarray-hub <yacoudiarra.wl@gmail.com>
2024-08-02 02:11:18 +0000

    Integrated Backend API

--- lib/login.dart
+++ lib/login.dart
@@ -1,12 +1,55 @@
-
 import 'package:asr_app/home.dart';
-
 import 'package:asr_app/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:asr_app/session.dart';
+import 'package:asr_app/bako_api/user.dart' show loginReaderUser;
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  void _login(BuildContext context, String username, String password) async {
+    // Check if username or password is empty
+    if (username.isEmpty || password.isEmpty) {
+      // Show a SnackBar informing the user to provide both username and password
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Username and password are required."),
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 3),
+        ),
+      );
+      return; // Exit the function early
+    }
+
+     await loginReaderUser(username, password).then((Map<String, dynamic>? response) {
+       if (response == null) {
+         print("Unsuccessful server resquest");
+       }
+       else if (response["status"]) {
+         Map<String, dynamic> userdata = response["data"];
+         SessionManager.setLoggedIn(username);
+         Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(userdata: userdata,)),);
+       }
+       else if (!response["status"]){
+         print("Login was not successful due to user error ${response["msg"]}");
+         // Show a SnackBar with the message from the server
+         ScaffoldMessenger.of(context).showSnackBar(
+           SnackBar(
+             content: Text("Login failed: ${response["msg"]}"),
+             backgroundColor: Colors.red,
+             duration: const Duration(seconds: 5),
+           ),
+         );
+       }
+     }
+     ); // End then()
+  }
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -24,9 +67,9 @@ class LoginPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               const Text(
-                'Se connecter',
+                'Login',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.deepPurple,
@@ -34,9 +77,10 @@ class LoginPage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 30),
-              const TextField(
-                decoration: InputDecoration(
+              const SizedBox(height: 30),
+              TextField(
+                controller: usernameController,
+                decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.email, color: Colors.deepPurple),
                   hintText: 'Username',
                   hintStyle: TextStyle(color: Colors.black),
@@ -52,13 +96,14 @@ class LoginPage extends StatelessWidget {
                     borderSide: BorderSide(color: Colors.white),
                   ),
                 ),
-                style: TextStyle(color: Colors.white),
+                style: const TextStyle(color: Colors.white),
                 cursorColor: Colors.white,
               ),
-              SizedBox(height: 20),
-              const TextField(
+              const SizedBox(height: 20),
+              TextField(
+                controller: passwordController,
                 obscureText: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.lock, color: Colors.deepPurple),
                   hintText: 'Password',
                   hintStyle: TextStyle(color: Colors.black),
@@ -74,42 +119,39 @@ class LoginPage extends StatelessWidget {
                     borderSide: BorderSide(color: Colors.white),
                   ),
                 ),
-                style: TextStyle(color: Colors.white),
+                style: const TextStyle(color: Colors.white),
                 cursorColor: Colors.white,
               ),
-              SizedBox(height: 10),
+              // Forgot password text button
+              const SizedBox(height: 10),
               Align(
                 alignment: Alignment.centerRight,
                 child: TextButton(
                   onPressed: () {
-                    
-                  },
+                    },
                   child: const Text(
-                    'Mot de passe oublié',
+                    'Forgot password?',
                     style: TextStyle(color: Colors.deepPurple),
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                 
-                },
+                onPressed: () => _login(context, usernameController.text, passwordController.text),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.transparent,
-                  side: BorderSide(color: Colors.white, width: 2),
-                  padding: EdgeInsets.symmetric(vertical: 15),
+                  side: const BorderSide(color: Colors.white, width: 2),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
                 child: const Text(
-                  'Se connecter',
+                  'Login',
                   style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               const Row(
                 children: [
                   Expanded(
@@ -121,7 +163,7 @@ class LoginPage extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 10.0),
                     child: Text(
-                      'ou',
+                      'or',
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
@@ -133,24 +175,24 @@ class LoginPage extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                 Navigator.push(context, MaterialPageRoute(builder: (context)=>Inscription()));
+                 Navigator.push(context, MaterialPageRoute(builder: (context)=> const Inscription()));
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.deepPurple,
-                  padding: EdgeInsets.symmetric(vertical: 15),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
                 child: const Text(
-                  "S'inscrire",
+                  "Sign up",
                   style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
               ),
-              SizedBox(height: 50),
+              const SizedBox(height: 50),
             ],
           ),
         ),
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,11 +1,54 @@
 import 'package:asr_app/login.dart';
 import 'package:flutter/material.dart';
+import 'package:asr_app/bako_api/user.dart' show createReaderAccount;
 
 class Inscription extends StatelessWidget {
   const Inscription({super.key});
 
+  void _signup(BuildContext context, String username, String password, {String? firstname, String? surname}) async {
+    // Check if username or password is empty
+    if (username.isEmpty || password.isEmpty) {
+      // Show a SnackBar informing the user to provide both username and password
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Please enter both username and password."),
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 3),
+        ),
+      );
+      return; // Exit the function early
+    }
+    // Account creation
+    await createReaderAccount(username, password,
+        firstname: firstname, surname: surname).then((Map<String, dynamic>? response) {
+              if (response == null) {
+                print("Unsuccessful server resquest");
+              }
+              else if (response["status"]) {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()),);
+              }
+              else if (!response["status"]){
+                print("Login was not successful due to user error ${response["msg"]}");
+                // Show a SnackBar with the message from the server
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Login failed: ${response["msg"]}"),
+                    backgroundColor: Colors.red,
+                    duration: const Duration(seconds: 5),
+                  ),
+                );
+              }
+            }
+            ); // End then()
+  }
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    final TextEditingController firstnameController = TextEditingController();
+    final TextEditingController surnameController = TextEditingController();
+
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -18,13 +61,13 @@ class Inscription extends StatelessWidget {
           ),
         ),
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizedBox(height: 50,),
-            Text("Inscription",
+              const SizedBox(height: 50,),
+            const Text("Sign up",
             textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.deepPurple,
@@ -33,11 +76,12 @@ class Inscription extends StatelessWidget {
               
             ),
             ),
-            SizedBox(height: 50,),
+            const SizedBox(height: 50,),
             TextField(
-              decoration: InputDecoration(
+              controller: firstnameController,
+              decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.person, color: Colors.deepPurple,),
-                hintText: "Firstname",
+                hintText: "Firstname (Optional)",
                 hintStyle: TextStyle(color:Colors.black),
                 filled: true,
                   fillColor: Colors.transparent,
@@ -52,11 +96,12 @@ class Inscription extends StatelessWidget {
                   ),
               ),
             ),
-            SizedBox(height: 40,),
+            const SizedBox(height: 40,),
            TextField(
-              decoration: InputDecoration(
+             controller: surnameController,
+              decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.person, color: Colors.deepPurple,),
-                hintText: "Surname",
+                hintText: "Surname (Optinal)",
                 hintStyle: TextStyle(color:Colors.black),
                 filled: true,
                   fillColor: Colors.transparent,
@@ -71,11 +116,12 @@ class Inscription extends StatelessWidget {
                   ),
               ),
             ),
-            SizedBox(height: 40,),
+            const SizedBox(height: 40,),
             TextField(
-              decoration: InputDecoration(
+              controller: usernameController,
+              decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.mail, color: Colors.deepPurple,),
-                hintText: "Username",
+                hintText: "Username*",
                 hintStyle: TextStyle(color:Colors.black),
                 filled: true,
                   fillColor: Colors.transparent,
@@ -90,9 +136,10 @@ class Inscription extends StatelessWidget {
                   ),
               ),
             ),
-            SizedBox(height: 40,),
+            const SizedBox(height: 40,),
             TextField(
-              decoration: InputDecoration(
+              controller: passwordController,
+              decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.lock, color: Colors.deepPurple,),
                 hintText: "Password",
                 hintStyle: TextStyle(color:Colors.black),
@@ -109,27 +156,25 @@ class Inscription extends StatelessWidget {
                   ),
               ),
             ),
-            
-             SizedBox(height: 50),
+             const SizedBox(height: 50),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
-                 },
+                onPressed: () => _signup(context, usernameController.text, passwordController.text,
+                    firstname: firstnameController.text, surname: surnameController.text),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.transparent,
-                  side: BorderSide(color: Colors.white, width: 2),
-                  padding: EdgeInsets.symmetric(vertical: 15),
+                  side: const BorderSide(color: Colors.white, width: 2),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
                 child: const Text(
-                  'S\'inscrire',
+                  'Sign up',
                   style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
               ),
               
-              SizedBox(height: 20,)
+              const SizedBox(height: 20,)
           
           ],),
         ),
