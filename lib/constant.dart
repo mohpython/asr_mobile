eef4146a1c8722d1e302d114194b8f55c14c1b08
diarray-hub <yacoudiarra.wl@gmail.com>
2024-08-02 02:10:04 +0000

    Added and reformatted constants

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,12 +1,30 @@
+const String bakoApiUri = 'http://127.0.0.1:8000';
+
+// We will probably get ride of those for a more scalable approach if application is deployed and maintained
+const List<String> titles = ['A-ABCDaire', 'A-Denmisɛnya Kojuguw', 'A-Donfɛnw', 'A-Kulɔriw', 'B-Anw bɛ Baara Kɛ !', 'B-Baganw', 'B-Bama Miirina',
+  'B-Sɔminiminɛnw', 'C-Jate', 'C-Kurun', 'C-Ne ni Mama ka Gafe Kalan', 'C-Ne ni Papa ka Gafe kalan', 'C-Nsbanji', 'C-Ni a tun bɛ se...',
+  'C-Tulonkɛw', 'D-Fali Nalonma Ni Ba Kegunma', 'D-Kɛwale Ɲumanw', 'D-Namasatigi', 'D-Kogo', 'D-Sonsan Tulogɛlɛn', 'D-Yɛlɛ ka di Npogotiginin min y'];
 
 const List<Map<String, String>> books = [
-  {'name': 'Book 1', 'logo': 'assets/images/book_1.png', 'url': 'database_url_1'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  {'name': 'Book 2', 'logo': 'assets/images/book_1.png', 'url': 'database_url_2'},
-  
+  {'title': 'A-ABCDaire', 'image': 'assets/images/book_1.png'},
+  {'title': 'A-Denmisɛnya Kojuguw', 'image': 'assets/images/book_1.png'},
+  {'title': 'A-Donfɛnw', 'image': 'assets/images/book_1.png'},
+  {'title': 'A-Kulɔriw', 'image': 'assets/images/book_1.png'},
+  {'title': 'B-Anw bɛ Baara Kɛ !', 'image': 'assets/images/book_1.png'},
+  {'title': 'B-Baganw', 'image': 'assets/images/book_1.png'},
+  {'title': 'B-Bama Miirina', 'image': 'assets/images/book_1.png'},
+  {'title': 'B-Sɔminiminɛnw', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Jate', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Kurun', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Ne ni Mama ka Gafe Kalan', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Ne ni Papa ka Gafe kalan', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Nsbanji', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Ni a tun bɛ se...', 'image': 'assets/images/book_1.png'},
+  {'title': 'C-Tulonkɛw', 'image': 'assets/images/book_1.png'},
+  {'title': 'D-Fali Nalonma Ni Ba Kegunma', 'image': 'assets/images/book_1.png'},
+  {'title': 'D-Kɛwale Ɲumanw', 'image': 'assets/images/book_1.png'},
+  {'title': 'D-Namasatigi', 'image': 'assets/images/book_1.png'},
+  {'title': 'D-Kogo', 'image': 'assets/images/book_1.png'},
+  {'title': 'D-Sonsan Tulogɛlɛn', 'image': 'assets/images/book_1.png'},
+  {'title': 'D-Yɛlɛ ka di Npogotiginin min y', 'image': 'assets/images/book_1.png'}
 ];
