0262856a47d5b052321ef3919ae9b57c4b0204cd
diarray-hub <yacoudiarra.wl@gmail.com>
2024-08-06 19:51:30 +0000

    Update constant.dart | add final constants titles and books & change bakoApiUri for 10.0.2.2:8000

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,5 @@
-const String bakoApiUri = 'http://127.0.0.1:8000';
+// This is only usable with android emulated devices running on the same machine than local server
+const String bakoApiUri = 'http://10.0.2.2:8000';
 
 // We will probably get ride of those for a more scalable approach if application is deployed and maintained
 const List<String> titles = ['A-ABCDaire', 'A-Denmisɛnya Kojuguw', 'A-Donfɛnw', 'A-Kulɔriw', 'B-Anw bɛ Baara Kɛ !', 'B-Baganw', 'B-Bama Miirina',
@@ -6,25 +7,25 @@ const List<String> titles = ['A-ABCDaire', 'A-Denmisɛnya Kojuguw', 'A-Donfɛnw'
   'C-Tulonkɛw', 'D-Fali Nalonma Ni Ba Kegunma', 'D-Kɛwale Ɲumanw', 'D-Namasatigi', 'D-Kogo', 'D-Sonsan Tulogɛlɛn', 'D-Yɛlɛ ka di Npogotiginin min y'];
 
 const List<Map<String, String>> books = [
-  {'title': 'A-ABCDaire', 'image': 'assets/images/book_1.png'},
-  {'title': 'A-Denmisɛnya Kojuguw', 'image': 'assets/images/book_1.png'},
-  {'title': 'A-Donfɛnw', 'image': 'assets/images/book_1.png'},
-  {'title': 'A-Kulɔriw', 'image': 'assets/images/book_1.png'},
-  {'title': 'B-Anw bɛ Baara Kɛ !', 'image': 'assets/images/book_1.png'},
-  {'title': 'B-Baganw', 'image': 'assets/images/book_1.png'},
-  {'title': 'B-Bama Miirina', 'image': 'assets/images/book_1.png'},
-  {'title': 'B-Sɔminiminɛnw', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Jate', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Kurun', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Ne ni Mama ka Gafe Kalan', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Ne ni Papa ka Gafe kalan', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Nsbanji', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Ni a tun bɛ se...', 'image': 'assets/images/book_1.png'},
-  {'title': 'C-Tulonkɛw', 'image': 'assets/images/book_1.png'},
-  {'title': 'D-Fali Nalonma Ni Ba Kegunma', 'image': 'assets/images/book_1.png'},
-  {'title': 'D-Kɛwale Ɲumanw', 'image': 'assets/images/book_1.png'},
-  {'title': 'D-Namasatigi', 'image': 'assets/images/book_1.png'},
-  {'title': 'D-Kogo', 'image': 'assets/images/book_1.png'},
-  {'title': 'D-Sonsan Tulogɛlɛn', 'image': 'assets/images/book_1.png'},
-  {'title': 'D-Yɛlɛ ka di Npogotiginin min y', 'image': 'assets/images/book_1.png'}
+  {'title': 'A-ABCDaire', 'image': 'assets/Thumbnails/ABCD-256.png'},
+  {'title': 'A-Denmisɛnya Kojuguw', 'image': 'assets/Thumbnails/DenmisenyaKojuguw-256.png'},
+  {'title': 'A-Donfɛnw', 'image': 'assets/Thumbnails/Donfenw-256.png'},
+  {'title': 'A-Kulɔriw', 'image': 'assets/Thumbnails/Kuloriw-256.png'},
+  {'title': 'B-Anw bɛ Baara Kɛ !', 'image': 'assets/Thumbnails/AnwBeBaaraKe!-256.png'},
+  {'title': 'B-Baganw', 'image': 'assets/Thumbnails/Baganw-256.png'},
+  {'title': 'B-Bama Miirina', 'image': 'assets/Thumbnails/BamaMiirina-256.png'},
+  {'title': 'B-Sɔminiminɛnw', 'image': 'assets/Thumbnails/Sominiminenw-256.png'},
+  {'title': 'C-Jate', 'image': 'assets/Thumbnails/Jate-256.png'},
+  {'title': 'C-Kurun', 'image': 'assets/Thumbnails/Kurun-256.png'},
+  {'title': 'C-Ne ni Mama ka Gafe Kalan', 'image': 'assets/Thumbnails/NeNiMamaKaGafeKalan-256.png'},
+  {'title': 'C-Ne ni Papa ka Gafe kalan', 'image': 'assets/Thumbnails/NeNiPapaKaGafeKalan-256.png'},
+  {'title': 'C-Nsbanji', 'image': 'assets/Thumbnails/Nsabanji-256.png'},
+  {'title': 'C-Ni a tun bɛ se...', 'image': 'assets/Thumbnails/NiATunBeSe-256.png'},
+  {'title': 'C-Tulonkɛw', 'image': 'assets/Thumbnails/Tulonkew-256.png'},
+  {'title': 'D-Fali Nalonma Ni Ba Kegunma', 'image': 'assets/Thumbnails/FaliNalonmaNiBaKegunma-256.png'},
+  {'title': 'D-Kɛwale Ɲumanw', 'image': 'assets/Thumbnails/KewaleNumanw-256.png'},
+  {'title': 'D-Namasatigi', 'image': 'assets/Thumbnails/Namasatigi-256.png'},
+  {'title': 'D-Kogo', 'image': 'assets/Thumbnails/Kogo-256.png'},
+  {'title': 'D-Sonsan Tulogɛlɛn', 'image': 'assets/Thumbnails/SonsanTulogelen-256.png'},
+  {'title': 'D-Yɛlɛ ka di Npogotiginin min y', 'image': 'assets/Thumbnails/YeleKaDiNpogotigininMiYe-256.png'}
 ];
