1b4f84983e32593c4eeb4be9545f5949942012a2
diarray-hub <yacoudiarra.wl@gmail.com>
2024-08-06 19:48:05 +0000

    Move book.dart | lib/bako_api/book.dart => lib/book.dart

---
+++ lib/book.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+/// Retrieves a specific book by its title from the catalog in books.json.
+///
+/// Parameters:
+/// - `title`: The title of the book to retrieve.
+///
+/// Returns a `Map<String, dynamic>?` containing the book details
+/// if the book is successfully retrieved, or `null` otherwise.
+Future<Map<String, dynamic>?> getBook(String title) async{
+  String jsonString = await rootBundle.loadString('assets/books/books.json');
+
+  // Decode the JSON string into a list of maps
+  final List<dynamic> booksList = jsonDecode(jsonString);
+
+  // Find the book with the matching title
+  for (var book in booksList) {
+    if (book['title'] == title) {
+      return book as Map<String, dynamic>;
+    }
+  }
+
+  // Return null if the book is not found or an error occurred
+  return null;
+}
