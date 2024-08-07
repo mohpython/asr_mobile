54104bc7234a8e0092c2b2dcd9b6f9b8190076f3
diarray-hub <yacoudiarra.wl@gmail.com>
2024-08-07 21:34:21 +0000

    Update lesson.dart | merge calculate score & markBookAsCompleted

--- lib/bako_api/lesson.dart
+++ lib/bako_api/lesson.dart
@@ -34,7 +34,7 @@ Future<Map<String, dynamic>?> bookmark(String username, String bookTitle, String
   }
 }
 
-/// Marks a book as completed for a user.
+/// Marks a book as completed for a user and calculate his score.
 ///
 /// Sends a POST request to the book completion API endpoint to mark
 /// a specific book as completed for the given user.
@@ -42,44 +42,14 @@ Future<Map<String, dynamic>?> bookmark(String username, String bookTitle, String
 /// Parameters:
 /// - `username`: The username of the account.
 /// - `bookTitle`: The title of the book to mark as completed.
-///
-/// Returns a `Map<String, dynamic>?` containing the server's response data
-/// if the book is successfully marked as completed, or `null` otherwise.
-Future<Map<String, dynamic>?> markBookAsCompleted(String username, String bookTitle) async {
-  final response = await http.post(
-    Uri.parse('$bakoApiUri/book_completed/'),
-    headers: {'Content-Type': 'application/json'},
-    body: jsonEncode({
-      'username': username,
-      'book_title': bookTitle,
-    }),
-  );
-
-  if (response.statusCode == 200) {
-    // Decode the response body using UTF-8
-    final decodedString = utf8.decode(response.bodyBytes);
-    // Convert the decoded response to a Map
-    final Map<String, dynamic> data = jsonDecode(decodedString);
-    return data;
-  }
-}
-
-/// Calculates a score for a book reading session.
-///
-/// Sends a POST request to the score calculation API endpoint to calculate
-/// the user's score for reading a specific book, based on errors and reading time.
-///
-/// Parameters:
-/// - `username`: The username of the account.
-/// - `bookTitle`: The title of the book for which the score is being calculated.
 /// - `numErrors`: (Optional) The number of errors made during reading.
 /// - `readingTime`: (Optional) The time taken to read the book.
 ///
 /// Returns a `Map<String, dynamic>?` containing the server's response data
-/// with the calculated score if the operation is successful, or `null` otherwise.
-Future<Map<String, dynamic>?> calculateScore(String username, String bookTitle, {int? numErrors, double? readingTime}) async {
+/// if the book is successfully marked as completed, or `null` otherwise.
+Future<Map<String, dynamic>?> markBookAsCompleted(String username, String bookTitle, {int? numErrors, double? readingTime}) async {
   final response = await http.post(
-    Uri.parse('$bakoApiUri/calculate_score/'),
+    Uri.parse('$bakoApiUri/book_completed/'),
     headers: {'Content-Type': 'application/json'},
     body: jsonEncode({
       'username': username,
